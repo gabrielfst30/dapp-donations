ebeea54671cd020e9e0a842aeaf8a5e8a87b3809
gabrielfst30 <gabrielfst30@gmail.com>
2025-05-05 01:23:49 -0300

    feature: criando tipagem do context

---
+++ src/contexts/donationProvider/types.ts
@@ -0,0 +1,30 @@
+import { BigNumberish } from "ethers";
+import { ReactNode } from "react";
+
+export type DonationItem = {
+  id: BigNumberish;
+  donor: string;
+  value: BigNumberish;
+};
+
+export type DonationItemFormat = {
+  id: string;
+  donor: string;
+  value: string;
+};
+
+export type Props = {
+  children: ReactNode;
+}
+
+export type Web3Context = {
+  connectWallet: () => void;
+  disconnectWallet: () => void;
+  donate: (amount: string) => Promise<void>;
+  donations: DonationItemFormat[];
+  loadingDonate: boolean;
+  loadingDonations: boolean;
+  showConfetti: boolean;
+  isConnected: boolean;
+  total: string;
+};
