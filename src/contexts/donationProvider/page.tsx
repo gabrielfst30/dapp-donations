3839e96a25a418b3adde40f09e9a8adc11bb6699
gabrielfst30 <gabrielfst30@gmail.com>
2025-05-06 00:43:24 -0300

    fix: atualizando importaçoes do ethers.js para utilizar o provider

--- src/contexts/donationProvider/page.tsx
+++ src/contexts/donationProvider/page.tsx
@@ -12,15 +12,36 @@ import {
 import { ethers, BigNumberish } from "ethers";
 import { DonationItemFormat, Props } from "./types";
 import { Web3ProviderContext } from "./web-context";
+import { BrowserProvider } from "ethers";
+
 
 export default function Web3Provider({ children }: Props) {
-  const [provider, setProvider] = useState<ReturnType<typeof Web3Provider>>();
+  const [provider, setProvider] = useState<BrowserProvider>();
   const [contract, setContract] = useState<ethers.Contract>();
   const [donations, setDonations] = useState<DonationItemFormat[]>([]);
   const [isConnected, setIsConnected] = useState(false);
   const [total, setTotal] = useState("0");
   const [loadingDonate, setLoadingDonate] = useState(false);
   const [loadingDonations, setLoadingDonations] = useState(false);
+
+  //Validando se o metamask esta instalado
+  useEffect(() => {
+    if (window.ethereum) {
+      const newProvider = new BrowserProvider(window.ethereum);
+      setProvider(newProvider);
+    }
+  }, []);
+
+  const connectWallet = async () => {
+    try {
+      const accounts = await provider?.send("eth_requestAccounts", []);
+      if (accounts?.length) {
+        setIsConnected(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Web3ProviderContext.Provider value={{}}>
