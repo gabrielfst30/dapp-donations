706d032110266c6f6d3b28ab4c221443c9d57cde
gabrielfst30 <gabrielfst30@gmail.com>
2025-05-07 00:39:04 -0300

    feature: criando useContext

--- src/contexts/donationProvider/web-context.ts
+++ src/contexts/donationProvider/web-context.ts
@@ -1,4 +1,4 @@
-import { createContext } from "react"; //função do React usada para criar um contexto.
+import { createContext, useContext } from "react"; //função do React usada para criar um contexto.
 import { Web3Context } from "./types"; //Tipagem de dados que iremos utilizar baseado no donation.sol
 
 
@@ -10,7 +10,10 @@ export const Web3ProviderContext = createContext<Web3Context>({
       donations:[],
       loadingDonate: false,
       loadingDonations: false,
-      showConfetti: false,
+      // showConfetti: false,
       isConnected: false,
       total: "0",
 })
+
+//hook customizado que você usa em componentes filhos para acessar o contexto sem precisar importar diretamente useContext e Web3ProviderContext.
+export const useWeb3 = () => useContext(Web3ProviderContext)
