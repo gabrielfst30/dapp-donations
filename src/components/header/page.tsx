003b65cc53d89270fb2aecb7d80224637f809e6d
gabrielfst30 <gabrielfst30@gmail.com>
2025-05-09 01:12:06 -0300

    fix: corrigindo responsividade

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -5,27 +5,33 @@ import Image from "next/image";
 export default function Header() {
   const { connectWallet, isConnected } = useWeb3();
 
-  console.log(isConnected)
-  
+  console.log(isConnected);
+
   return (
-    <header className="flex flex-row w-full h-30 pt-10 bg-transparent items-center justify-between pl-80 pr-80">
+    <header className="flex flex-col sm:flex-row w-full py-6 px-4 lg:px-20 bg-transparent items-center justify-between gap-4">
       {/* Seção da Esquerda */}
       <div className="flex items-center gap-2">
         {/* Title */}
-        <h1 className="text-4xl font-bold">Penguin Donation</h1>
+        <h1 className="text-3xl sm:text-4xl font-bold text-white">
+          Penguin Donation
+        </h1>
 
         {/* Logo */}
         <Image
           alt="logo de penguin"
           src="/penguinho.png"
-          width={60}
-          height={60}
+          width={50}
+          height={50}
+          className="object-contain"
         />
       </div>
+
       {/* Seção da Direita */}
       <div className="flex items-center">
-        {/* Button */}
-        <button className="w-fit p-2 text-xl rounded-md border-2 border-white text-white hover:bg-white hover:text-black hover:border-black" onClick={connectWallet}>
+        <button
+          className="w-fit px-4 py-2 text-base sm:text-xl rounded-md border-2 border-white text-white hover:bg-white hover:text-black hover:border-black"
+          onClick={connectWallet}
+        >
           {!isConnected ? "Conectar" : "Conectado"}
         </button>
       </div>
